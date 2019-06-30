df5441097cce95897a4a93c75eb85595a63f0e2f
Tayte Stokes <taytestokes9328@gmail.com>
2019-06-30 09:10:26 -0600

    setup client login page

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const passport = require('passport')
+const sessions = require('express-session');
+const massive = require('massive');
+// Passport Strategies
+const LocalStrategy = require('passport-local');
+
+// App Setup
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(sessions({
+    secret: 'passport auth examples 🚀'
+}));
+// need to use the passport initialize middleware to get passport working
+app.use(passport.initialize());
+
+
+// Database Connection
+
+
+/* --- Passport --- */
--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 
+// CSS
+import 'reset-css';
+import './styles/App.css';
+
 class App extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="app-container">
+        <form className="form-container">
+          <input type="email" placeholder="email"/>
+          <input type="password" placeholder="password"/>
+          <button>Login</button>
+          <button>Register</button>
+          <button>Gmail</button>
+          <button>Twitter</button>
+          <button>Github</button>
+          <button>Facebook</button>
+        </form>
       </div>
     )
   }
